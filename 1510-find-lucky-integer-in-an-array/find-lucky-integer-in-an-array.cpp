98c81015d5ff5ba19ac182404d2e1715b9b7ed9f
Karan Bhosale <karanbhosale1226@gmail.com>
2026-09-15 23:56:53 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (24.63%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int findLucky(vector<int>& arr) {
         unordered_map<int,int> mp;
-        sort(arr.begin(),arr.end());
         int n=arr.size();
         for(int i=0;i<n;i++){
             mp[arr[i]]++;
@@ -10,7 +9,9 @@ public:
         int ans=-1;
         for(int i=0;i<n;i++){
             if(mp[arr[i]]==arr[i]){
-                ans= arr[i];
+                if(arr[i]>ans){
+                    ans= arr[i];
+                }
             }
         }
         return ans;
