1fb84ba68889999f806ed71b1210289864d965a1
Karan Bhosale <karanbhosale1226@gmail.com>
2026-09-17 00:10:27 +0530

    Time: 4 ms (22.71%) | Memory: 14 MB (72.34%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    bool ispresent(int n,vector<int> ans){
-        for(int i=0;i<ans.size();i++){
-            if(n==ans[i]){
-                return true;
-            }
-        }
-        return false;
-    }
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int> ans;
-        for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    if(!ispresent(nums1[i],ans)){
-                        ans.push_back(nums1[i]);
-                    }
-                }
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int i=0;
+        int j=0;
+        while(i<nums1.size() && j<nums2.size()){
+            if((nums1[i]==nums2[j]) && (ans.empty() || ans.back()!=nums1[i])){
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i]>nums2[j]){
+                j++;
+            }
+            else{
+                i++;
             }
         }
         return ans;
